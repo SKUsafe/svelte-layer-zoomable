52cf581cd80b0ab955366879274734a01a6e3f7b
Yomkool <bannon.pj@gmail.com>
2024-05-17 09:49:06 -0400

    changies

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,19 @@
-import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: preprocess(),
-
   kit: {
     adapter: adapter(),
-    paths: { base: "/svelte-layer-zoomable" },
+    paths: { base: '/svelte-layer-zoomable' },
     prerender: {
       default: true,
     },
     package: {
-      dir: "dist",
+      dir: 'dist',
+      exports: (file) => file === 'index.js', // Ensure this matches your entry file
+      files: (file) => !file.startsWith('dev') // Include necessary files
     },
   },
 };
